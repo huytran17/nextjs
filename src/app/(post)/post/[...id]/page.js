9bcb2404a846d4c0171ea906a1d05b42186b16bb
huytran17 <alpha.lloyd1368@gmail.com>
2023-07-23 16:21:06 +0700

    SEO test

--- src/app/(post)/post/[...id]/page.js
+++ src/app/(post)/post/[...id]/page.js
@@ -1,10 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
 export default function SpreadParams({ params, url }) {
-  const router = useRouter();
-
   return (
     <div>
       <h1>There are a set of route params: {params.id.join(", ")}</h1>
--- src/app/(post)/post/[id]/page.js
+++ src/app/(post)/post/[id]/page.js
@@ -1,15 +1,20 @@
-"use client";
-
+import Link from "next/link";
 import { posts } from "@/data";
-import { useRouter } from "next/navigation";
+
+export const generateMetadata = ({ params }) => {
+  const post = posts.find((post) => post.id === params.id);
+
+  return {
+    title: post.title,
+  };
+};
 
 export default function Post({ params }) {
-  const router = useRouter();
-
   const post = posts.find((post) => post.id === params.id);
+
   return (
-    <h1 onClick={() => router.push("/post/abc/def/ghi")}>
-      This is post: {post.title}
+    <h1>
+      <Link href="/post/abc/def/ghi">This is post: {post.title}</Link>
     </h1>
   );
 }
