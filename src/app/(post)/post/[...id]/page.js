d7edb1037896137a77e75d298811521de6ab7338
huytran17 <alpha.lloyd1368@gmail.com>
2023-07-23 15:56:08 +0700

    loading state

--- src/app/(post)/post/[...id]/page.js
+++ src/app/(post)/post/[...id]/page.js
@@ -8,14 +8,6 @@ export default function SpreadParams({ params, url }) {
   return (
     <div>
       <h1>There are a set of route params: {params.id.join(", ")}</h1>
-
-      <h2 onClick={() => router.push("/post/optional-param/qwe/rty")}>
-        Optional: has params
-      </h2>
-
-      <h2 onClick={() => router.push("/post/optional-param")}>
-        Optional: has no params
-      </h2>
     </div>
   );
 }
